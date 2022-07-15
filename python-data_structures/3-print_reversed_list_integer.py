cf9b5b91b5c4f3db779179e4633ee0dfeb9d75ea
ell-sie <e.ndiramiye@alustudent.com>
2022-07-15 07:12:29 -0700

    data structures

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-def replace_in_list(my_list, idx, element):
-    if idx < 0:
-        return my_list
-    elif idx >= len(my_list):
-        return my_list
-    else:
-        my_list[idx] = element
-        return my_list
+def print_reversed_list_integer(my_list=[]):
+    if isinstance(my_list, list):
+        count = len(my_list) - 1
+        if count >= 0:
+            for i in my_list:
+                print("{:d}".format(my_list[count]))
+                count -= 1
