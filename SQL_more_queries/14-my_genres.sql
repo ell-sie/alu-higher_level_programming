892ede07648c4bbeed5fa75d26941092716a8365
ell_sie <e.ndiramiye@alustudent.com>
2022-08-06 21:13:39 +0000

    watawi

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,9 +1,9 @@
--- lists all genre of show Dexter
-SELECT tv_genres.name
-FROM tv_genres
-JOIN tv_show_genres 
-ON tv_genres.id = tv_show_genres.genre_id
-JOIN tv_shows
-ON tv_shows.id = tv_show_genres.show_id
-WHERE tv_gen = 'Dexter'
+-- my genres
+SELECT tv_genres.name 
+FROM tv_shows 
+INNER JOIN tv_show_genres 
+ON tv_shows.id = tv_show_genres.show_id 
+INNER JOIN tv_genres 
+ON tv_show_genres.genre_id = tv_genres.id 
+WHERE tv_shows.title='Dexter' 
 ORDER BY tv_genres.name;
